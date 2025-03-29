ebb3621d7a139af8d11ae43b1da85ff2e966cc2e
LittleMercyEh <liam@portent.ca>
2025-03-28 22:40:52 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import HousePricePredictor from './components/HousePricePredictor';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/predict" element={<HousePricePredictor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
